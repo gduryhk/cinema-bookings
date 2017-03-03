6332373d1d11834912de8b844329fca4fb728f67
gduryhk <guillaume@dury.pro>
2017-03-03 17:59:58 +0800

    Modifying the rest of the files again

--- controllers/resourses.go
+++ controllers/resourses.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	"github.com/mmorejon/cinema/bookings/models"
+	"github.com/gdury/cinema-bookings/models"
 )
 
 type (
--- data/bookingRepository.go
+++ data/bookingRepository.go
@@ -1,7 +1,7 @@
 package data
 
 import (
-	"github.com/mmorejon/cinema/bookings/models"
+	"github.com/gdury/cinema-bookings/models"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
